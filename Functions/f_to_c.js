b3533cf62ad4f73a9142ae52df5f0fac147afc53
AmyRobrigado <amyantanod@gmail.com>
2022-10-24 15:41:32 -0400

    Added new js file

---
+++ Functions/f_to_c.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function convertFtoC(currentTemp, celsiusTemp)
+{
+    let fahrenheit = 92;
+    let celsius = 5/9 - 32;
+
+    console.log("The current temperature is " + );
+}
+
+displayconverFtoC()
