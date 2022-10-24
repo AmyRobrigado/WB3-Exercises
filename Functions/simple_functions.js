1dec053954e467d6c806bc941c1d541b53a30a2f
AmyRobrigado <amyantanod@gmail.com>
2022-10-24 13:45:56 -0400

    Modified js file

--- Functions/simple_functions.js
+++ Functions/simple_functions.js
@@ -1,24 +1,21 @@
 "use strict";
 
 /* 1 */
-function favoriteThings(name, favoriteMovie, favoriteMusician) 
-{
-     let thename = "Amy";
-     let myfavoriteMovie = "Princess and the Frog";
-     let myfavoriteMusician = "Kenshi Yonezu";
-
-    console.log("Hello my name is " + thename + " my favorite movie is " 
-    + myfavoriteMovie + " and my favorite musician is " + myfavoriteMusician);
+function favoriteThings(thename, myfavoriteMovie, myfavoriteMusician) {
+    console.log("Hello my name is " + thename + " my favorite movie is "
+        + myfavoriteMovie + " and my favorite musician is " + myfavoriteMusician);
 }
+favoriteThings("Amy", "Princess Mononoke", "Kenshi Yonezu");
 
 /* 2 */
-function whyImHere()
-{
-
+function whyImHere(message) {
+    console.log(message);
 }
+whyImHere("I am here to broaden my skill set");
 
 /* 3 */
-function favoritePlace()
-{
-
+function favoritePlace(message) {
+    console.log(message);
 }
+
+favoritePlace("My favorite place to be is Kinokuniya, I love shopping for books and hanging out in the cafe. ")
