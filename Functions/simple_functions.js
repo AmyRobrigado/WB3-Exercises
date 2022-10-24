cf2ea3ae215e5904d3b190bd5558024b045de5bf
AmyRobrigado <amyantanod@gmail.com>
2022-10-24 13:26:06 -0400

    Modified js file

---
+++ Functions/simple_functions.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/* 1 */
+function favoriteThings(name, favoriteMovie, favoriteMusician) 
+{
+     let thename = "Amy";
+     let myfavoriteMovie = "Princess and the Frog";
+     let myfavoriteMusician = "Kenshi Yonezu";
+
+    console.log("Hello my name is " + thename + " my favorite movie is " 
+    + myfavoriteMovie + " and my favorite musician is " + myfavoriteMusician);
+}
+
+/* 2 */
+function whyImHere()
+{
+
+}
+
+/* 3 */
+function favoritePlace()
+{
+
+}
