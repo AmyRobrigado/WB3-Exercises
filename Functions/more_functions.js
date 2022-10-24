096135251badbdc2e09d7d3a509715deb3e69854
AmyRobrigado <amyantanod@gmail.com>
2022-10-24 15:30:42 -0400

    Modified more_functions js file

--- Functions/more_functions.js
+++ Functions/more_functions.js
@@ -8,6 +8,9 @@ function displayMailingLabel(name, address, city, state, zip) {
 }
 
 displayMailingLabel("Amy", "231-11 Haverick St", "Dankridge", "New York", "44212");
+displayMailingLabel("Betty", "231-11 Haverick St", "Dankridge", "New York", "44212");
+displayMailingLabel("Sherry", "231-11 Haverick St", "Dankridge", "New York", "44212");
+
 
 /* Add Numbers */
 function addNumbers(num1, num2) {
@@ -17,18 +20,25 @@ function addNumbers(num1, num2) {
 
 addNumbers(34, 10);
 addNumbers(12, 40);
+addNumbers(13, 24);
 
 /* Receipt */
-function displayReceipt(totalDue, amountPaid) {
+function displayReceipt(totalDue, amountPaid, changeDue) {
     let due = totalDue;
     let paid = amountPaid;
-    let change = totalDue - amountPaid;
+    let change = changeDue;
 
-    console.log("Total Due: " + due);
-    console.log("Amount Due: " + paid);
-    console.log("Change Due: " + change);
+    console.log("Total Due: " + due + ", Amount Due: " + paid + " Change Due: " + change);
+    
 }
-displayReceipt("$" + 300.00);
+displayReceipt("$" + 300.00, "$" + 300.00, "$" + 0);
+displayReceipt("$" + 300.00, "$" + 400.00, "$" + 0);
+displayReceipt("$" + 300.00, "$" + 240.00, "$" + 140.00);
+
+
+
+
+
 
 
 
