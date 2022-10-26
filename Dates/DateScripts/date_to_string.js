b0a671323b947fc9d54959f149f3f2944386c8cc
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-10-26 14:15:52 -0400

    Created new files and modified them

--- Dates/DateScripts/date_to_string.js
+++ Dates/DateScripts/date_to_string.js
@@ -2,19 +2,70 @@
 
 window.onload = init;
 
-function init()
-{
+//1
+function init() {
     const confirBtn = document.getElementById("confirBtn");
-    confirBtn.onclick = confirBtnOnClicked; 
+    confirBtn.onclick = confirBtnOnClicked;
 }
 
-function confirBtnOnClicked()
-{
-    const confirm = document.getElementById("dateIpt")
-    
+function confirBtnOnClicked() {
+    const confirm = document.getElementById("dateIpt");
+
     let newDate = new Date(confirm.value);
 
     let dateAlr = document.getElementById("dateAlr");
+    //toString()
     dateAlr.innerHTML = newDate.toString();
-
 }
+
+//2
+function init() {
+    const confirBtn = document.getElementById("confirBtn");
+    confirBtn.onclick = confirBtnOnClicked;
+}
+
+
+function confirBtnOnClicked() {
+    const confirm = document.getElementById("dateIpt")
+
+    let newDate = new Date(confirm.value);
+
+    let dateAlr = document.getElementById("dateAlr");
+    //toDateString()
+    dateAlr.innerHTML = newDate.toDateString();
+}
+
+//3
+function init() {
+    const confirBtn = document.getElementById("confirBtn");
+    confirBtn.onclick = confirBtnOnClicked;
+}
+
+
+function confirBtnOnClicked() {
+    const confirm = document.getElementById("dateIpt")
+
+    let newDate = new Date(confirm.value);
+
+    let dateAlr = document.getElementById("dateAlr");
+    //toUTCString()
+    dateAlr.innerHTML = newDate.toUTCString();
+}
+
+//4
+function init() {
+    const confirBtn = document.getElementById("confirBtn");
+    confirBtn.onclick = confirBtnOnClicked;
+}
+
+
+function confirBtnOnClicked() {
+    const confirm = document.getElementById("dateIpt")
+
+    let newDate = new Date(confirm.value);
+
+    let dateAlr = document.getElementById("dateAlr");
+    //toLocalString()
+    dateAlr.innerHTML = newDate.toLocaleString();
+}
+
---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// My Birthday
+
+    let d1 = new Date("09/16/1998"); // September 16, 1998
+    
+    console.log(d1.toLocaleString());
+
+// Mom's Birthday 
+     let d2 = new Date("July 10 1957");
+
+     console.log(d2.toLocaleString());
+
+// Dad's Birthday 
+    let d3 = new Date("1954 March 5");
+
+    console.log(d3.toLocaleString());
